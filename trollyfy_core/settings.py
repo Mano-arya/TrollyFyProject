6b4f8ea143fd3188f4e684da6a1dab3ff383f124
Madhav Dhungana <35417987+madhav-dhungana@users.noreply.github.com>
2026-06-02 22:22:17 +0900

    Update settings.py

--- trollyfy_core/settings.py
+++ trollyfy_core/settings.py
@@ -105,7 +105,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media files (User uploads)
 MEDIA_URL = '/media/'
