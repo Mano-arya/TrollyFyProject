57650faacadff0c5264be0eced865f82082c4121
Madhav Dhungana <35417987+madhav-dhungana@users.noreply.github.com>
2026-06-02 22:29:14 +0900

    Update settings.py

--- trollyfy_core/settings.py
+++ trollyfy_core/settings.py
@@ -9,10 +9,16 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
-SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-default-key-change-me')
+#SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-default-key-change-me')
+SECRET_KEY = os.environ["SECRET_KEY"]
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'testserver''https://trollyfyproject-866789048793.europe-west1.run.app/']
-CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1', 'http://localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'testserver','trollyfyproject-866789048793.europe-west1.run.app']
+#CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1', 'http://localhost']
+CSRF_TRUSTED_ORIGINS = [
+    origin.strip()
+    for origin in os.getenv("CSRF_TRUSTED_ORIGINS", "").split(",")
+    if origin.strip()
+]
 
 # Application definition
 INSTALLED_APPS = [
